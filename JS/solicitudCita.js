c6275e901c208706e145dcb1b41e589bbcd7ad03
Alexander <prodbydsceron@gmail.com>
2023-11-23 21:01:36 -0500

    Agregado código en solicitudCitas.js para conectar el formulario con la BD, creación de formulario y sus estilos y agregado código en conexionDB

--- JS/solicitudCita.js
+++ JS/solicitudCita.js
@@ -1,31 +1,42 @@
-const {connection} = require('./conexiondb');
+const {
+    connection
+} = require('./conexiondb');
 const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static('Proyecto Infocenter'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get('/',(req, res)=>{
-    const filePath = path.join(__dirname,'..','index.html');
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, '..', 'Proyecto Infocenter', 'index.html');
     res.sendFile(filePath);
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Servidor ejecutado en http://localhost:${PORT}`)
 })
 
-app.post('/solicitud/cita',(req, res)=>{
-    const {nombreMascota, razaMascota, edadMascota, fechaCita, horaCita, nombreDueno} = req.body;
+app.post('/solicitud/cita', (req, res) => {
+    const {
+        nombre_mascota,
+        raza_mascota,
+        edad_mascota,
+        fecha_cita,
+        hora_cita,
+        nombre_dueño
+    } = req.body;
 
     //creamos la consulta para la inserción de la base de datos
 
-    const sql = 'INSERT INTO citas (nombre_mascota, raza_mascota, edad_mascota, fecha_cita, hora_cita, nombre_dueno) VALUES (?,?,?,?,?,?)';
-    connection.query(sql, [nombreMascota, razaMascota, edadMascota, fechaCita, horaCita, nombreDueno], (err, result)=>{
-        if(err){
+    const sql = 'INSERT INTO citas (nombre_mascota, edad_mascota, raza_mascota, fecha_cita, hora_cita, nombre_dueño) VALUES (?,?,?,?,?,?)';
+    connection.query(sql, [nombre_mascota, raza_mascota, edad_mascota, fecha_cita, hora_cita, nombre_dueño], (err, result) => {
+        if (err) {
             throw err;
         }
         console.log('Registro Exitoso');
